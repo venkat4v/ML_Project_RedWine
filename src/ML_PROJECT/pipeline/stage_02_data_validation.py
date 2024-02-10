a750e95e57713528c195461a9911f0359d77a43c
Venkat Prasad Reddy Dharmala <VENKATPRASAD4V@GMAIL.COM>
2024-02-11 01:01:22 +0530

    Data Validation added

---
+++ src/ML_PROJECT/pipeline/stage_02_data_validation.py
@@ -0,0 +1,28 @@
+from ML_PROJECT.config.configuration import ConfigurationManager
+from ML_PROJECT.components.data_validation import DataValidation
+
+
+STAGE_NAME = "Data_Validation"
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
+    
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+    
