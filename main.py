206de2f3e64726b2a74957266a5683a2b0db3699
Venkat Prasad Reddy Dharmala <VENKATPRASAD4V@GMAIL.COM>
2024-02-10 23:48:53 +0530

    adding data_ingestion_pipeling

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from ML_PROJECT import logger
+from ML_PROJECT.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("My name is Venkat")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
